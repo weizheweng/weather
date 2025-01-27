e896ba143c6c2d8345e274c760d7785cdab3c7cf
Zack <zack@kapito.io>
2025-01-27 20:20:18 +0800

    test

--- src/constants/baseUrls.ts
+++ src/constants/baseUrls.ts
@@ -1,4 +1,4 @@
 // 定義的API網址
-export const WEATHER_GEO_API_URL = 'http://api.openweathermap.org/geo/1.0/direct'
+export const WEATHER_GEO_API_URL = 'https://api.openweathermap.org/geo/1.0/direct'
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
